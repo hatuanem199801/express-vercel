2723cb70504c25d31e293b1997f378a11049a749
Ha Tuan Em <hatuanem199801@gmail.com>
2021-07-04 21:30:23 +0700

    Deploy application

---
+++ api/product.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const product = require("./api/product");
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/product", product);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
