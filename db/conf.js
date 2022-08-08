713e72d639ae26e7c1a910e921da231a28abf4d2
Ismael Dominguez <ismael@uaeh.edu.mx>
2022-08-08 16:54:01 -0500

    saveUser post method

--- db/conf.js
+++ db/conf.js
@@ -1,13 +1,5 @@
 const mysql = require('mysql2');
 
-// const connection =  mysql.createConnection({
-//     host: 'so9ugde0k5xq.us-east-4.psdb.cloud',
-//     user: '68ol5y6h0tmy',
-//     password: 'pscale_pw_HypwxG0pxiZ3H15U_smC5IuubTLZdjqj9BuA14CCJCc',
-//     ssl:{}
-// });
-
-
 const connection = mysql.createConnection({
     host: process.env.PLANETSCALE_DB_HOST,
     user: process.env.PLANETSCALE_DB_USERNAME,
