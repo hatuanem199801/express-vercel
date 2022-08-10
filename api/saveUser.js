eca69390c176331419742ce8b4c3d82a089b6f65
Ismael Dominguez <ismael@uaeh.edu.mx>
2022-08-10 14:26:00 -0500

    cors for post

--- api/saveUser.js
+++ api/saveUser.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
+var cors = require('cors')
 connection = require("../db/conf");
 let message;
 const app = express();
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 // app.use(bodyParser.raw());
@@ -13,13 +15,6 @@ connection.connect();
 
 
 app.post('/', function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept",
-        "Access-Control-Allow-Origin", "*",
-        "Access-Control-Allow-Methods", "*"
-    );
     var nombre = req.body.nombre;
     var aPaterno = req.body.aPaterno;
     var aMaterno = req.body.aMaterno;
