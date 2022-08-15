8d3ae8519586707fa3f74328618168f3254514a4
Brian Cruz Sanchez <BrianCS1999@outlook.com>
2022-08-15 12:09:01 -0500

    connection added

--- api/saludo.js
+++ api/saludo.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const cors = require('cors')
+connection = require("../db/conf");
 const app = express();
 app.use(cors())
 
 router.get("/", async (req, res) => {
-  
-   res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
   try {
+    connection.connect();
+
     res.json({
       status: 200,
       message: "server is awake!!!1",
