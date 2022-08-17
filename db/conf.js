6c9b5e90dd19f59ac6b65935962cb8ff9cf6258b
Ismael Dominguez <ismael@uaeh.edu.mx>
2022-08-17 16:46:47 -0500

    correct config

--- db/conf.js
+++ db/conf.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
-const connection =  mysql.createConnection({
-    host: 'f450gmj9s3zg.us-east-1.psdb.cloud',
-    user: 'ndys7fttqowr',
-    password: 'pscale_pw_LIJLaV1DHORQ8bo3iOUzQgrsg-hiV5UrGo3W5goKiis',
+const connection = mysql.createConnection({
+    host: process.env.PLANETSCALE_DB_HOST,
+    user: process.env.PLANETSCALE_DB_USERNAME,
+    password: process.env.PLANETSCALE_DB_PASSWORD,
     ssl:{}
 });
 
